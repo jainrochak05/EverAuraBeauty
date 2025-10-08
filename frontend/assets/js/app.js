b610e9bc5e033ac9799a80d413c91fcc6ce43e7b
Rochak Kr. Jain <160702037+jainrochak05@users.noreply.github.com>
2025-10-09 00:53:54 +0530

    Update app.js

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://everaura.vercel.app/api';
+const API_URL = 'https://ever-aura-beauty.vercel.app/api';
 
 // --- DOM Initialization ---
 document.addEventListener('DOMContentLoaded', () => {
