a3dc4f9d82b357a2de78b791496e78a492e92cdb
Rochak Kr. Jain <160702037+jainrochak05@users.noreply.github.com>
2025-10-13 00:59:18 +0530

    Update dashboard.js

--- frontend/assets/js/dashboard.js
+++ frontend/assets/js/dashboard.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://ever-aura-beauty.vercel.app/api';
+const API_URL = 'https://everaurabeauty-backend.onrender.com/api';
 
 document.addEventListener('DOMContentLoaded', () => {
     if (document.getElementById('login-form')) {
